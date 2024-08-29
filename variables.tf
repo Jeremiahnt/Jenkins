e14ee0810d52d35a1071c3a6755789b4af4ebf48
Jeremiah F. <83379118+Jeremiahnt@users.noreply.github.com>
2024-08-29 09:12:33 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "azure-client-id" {
+  description = "client id"
+  type        = string
+}
+variable "azure-client-secret" {
+  description = "secret"
+  type        = string
+}
+variable "azure-tenant-id" {
+  description = "tenant id"
+  type        = string
+}
+variable "azure-subscription-id" {
+  description = "subscription target"
+  type        = string
+}
