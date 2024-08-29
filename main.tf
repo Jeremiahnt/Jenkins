2168bb028e5858c5b01396f34479f9d93c9a57d3
Jeremiah F. <83379118+Jeremiahnt@users.noreply.github.com>
2024-08-29 09:18:36 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  client_id       = "var.azure-client-id"
-  client_secret   = "var.azure-client-secret"
-  tenant_id       = "var.azure-tenant-id"
-  subscription_id = "var.azure-subscription-id"
 }
 
 resource "azurerm_resource_group" "IAC-rg" {
